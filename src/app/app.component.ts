5c23f1a71144649846e64b8a643c839036fb836d
KavyaVijayakumar <kvk38370@gmail.com>
2024-02-07 15:41:56 +0530

    reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive';
+export class AppComponent implements OnInit {
+  myForm: any;
+ 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      // Perform form submission logic here
+    } else {
+      // Handle form validation errors
+      console.log('Form is invalid');
+    }
+  }
 }
